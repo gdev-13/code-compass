acb341b5dfeef1b1221b1d91ab8718ab20471637
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-08-06 19:30:04 -0300

    chore: inicializar estrutura do backend

---
+++ backend/src/test/java/com/codecompass/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
