048599131234cfb34e9ffc00d2b8c7e7170d999e
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-08-10 22:38:32 -0300

    feat: adicionar endpoint de cadastro de usuário

---
+++ backend/src/main/java/com/codecompass/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.codecompass.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codecompass.dto.user.CreateUserRequest;
+import com.codecompass.dto.user.UserResponse;
+import com.codecompass.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final UserService userService;
+	
+	@PostMapping("/register")
+	public UserResponse register(@RequestBody CreateUserRequest request)  {
+		return userService.createUser(request);
+	}
+}
